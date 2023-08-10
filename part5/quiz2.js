db2119a9e67daee74aae1d4de03b20ff455ebdb7
fathurrohmanrizqi <kikiahong27@gmail.com>
2023-08-10 20:36:57 +0700

    push sampe bisa

---
+++ part5/quiz2.js
@@ -0,0 +1,61 @@
+// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+
+// Gunakan switch case untuk kasus ini!
+
+// Contoh:
+
+// let hari = 21; let bulan = 1; let tahun = 1945;
+
+// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+
+//tips gunakan keyword ini di google "conditional switch case javascript"
+//dan mulailah membaca dan mencontek di stackoverflow xixixi
+
+let tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+let bulan = 7; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+let tahun = 2004; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+//code switch case kamu disini
+let month = "";
+switch (bulan) {
+  case 1:
+    month = "January";
+    break;
+  case 2:
+    month = "February";
+    break;
+  case 3:
+    month = "March";
+    break;
+  case 4:
+    month = "April";
+    break;
+  case 5:
+    month = "May";
+    break;
+  case 6:
+    month = "June";
+    break;
+  case 7:
+    month = "July";
+    break;
+  case 8:
+    month = "August";
+    break;
+  case 9:
+    month = "September";
+    break;
+  case 10:
+    month = "October";
+    break;
+  case 11:
+    month = "November";
+    break;
+  case 12:
+    month = "December";
+  default:
+    month = "Tulis bulan dulu bang!!";
+    break;
+}
+
+console.log(`${tanggal} ${month} ${tahun}`);
